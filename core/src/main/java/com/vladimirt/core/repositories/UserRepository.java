07cb631c85beb2ada67b907c2408ad6216e34dbb
Vladimir Tikaev <vladimirt.215@gmail.com>
2019-11-18 15:47:04 +0300

    Create UserRepository interface

---
+++ core/src/main/java/com/vladimirt/core/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.vladimirt.core.repositories;
+
+import com.vladimirt.core.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> { //Передаем класс модель и тип id. Наследуемся от этого класса
+    // т.к. внутри есть все базовые CURD методы
+}
