1ae94aca7ef42516b811125c1e34abd6ca8bc4a7
Vladimir Tikaev <vladimirt.215@gmail.com>
2019-11-19 11:49:02 +0300

    Add Role and User DTO classes

---
+++ core/src/main/java/com/vladimirt/core/dto/UserDTO.java
@@ -0,0 +1,17 @@
+package com.vladimirt.core.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserDTO {
+
+    private Long id;
+    private String email;
+    private String password;
+
+
+}
