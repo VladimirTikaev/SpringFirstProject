5e05394084fd685e23171a2a970c32a803482061
Vladimir Tikaev <vladimirt.215@gmail.com>
2019-11-24 16:44:44 +0300

    Create Card class

---
+++ core/src/main/java/com/vladimirt/core/model/Card.java
@@ -0,0 +1,27 @@
+package com.vladimirt.core.model;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "cards")
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = {"id"})
+public class Card {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    private EnWord enWord;
+    @ManyToOne
+    private RuWord ruWord;
+    private Long rating;
+
+}
