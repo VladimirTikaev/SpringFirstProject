061cf07c505488086fc00ab030b54e3b7541db55
Vladimir Tikaev <vladimirt.215@gmail.com>
2019-11-24 16:38:14 +0300

    Create EnWordDTO class

---
+++ core/src/main/java/com/vladimirt/core/dto/EnWordDTO.java
@@ -0,0 +1,16 @@
+package com.vladimirt.core.dto;
+
+import lombok.*;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class EnWordDTO {
+
+    private Long id;
+    private String meaning;
+}
